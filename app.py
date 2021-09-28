d812a7b910b33e301259a7f6e31d59aa2525e1a9
ELkeJohannes <elkeharmanny@outlook.com>
2021-09-28 20:04:14 +0200

    Connect app to Mongodb

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from flask import render_template
 
 if os.path.exists("env.py"):
@@ -7,10 +11,17 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return render_template('home.html')
+    recipes = mongo.db.Recipes.find()
+    return render_template('home.html', recipes = recipes)
 
 
 if __name__ == "__main__":
--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "6#r*mr8N!kycWaWI4nX8GHaO^")
-#os.environ.setdefault("MONGO_URI", "")
-#os.environ.setdefault("MONGO_DBNAME", "task_manager")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:R00tUser1234@cluster0.bivzi.mongodb.net/RecipeToGrow?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "RecipeToGrow")
