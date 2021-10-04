295be4d8af91d902c6cfe96798a16c6a353260a7
ELkeJohannes <elkeharmanny@outlook.com>
2021-10-04 12:20:39 +0200

    Add register functionality

--- app.py
+++ app.py
@@ -30,19 +30,49 @@ def recipes():
     return render_template('recipes.html', recipes = recipes)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        USERNAME = request.form.get("username").lower()
+        
+        # check if username is already in use in db
+        existing_user = mongo.db.Users.find_one(
+            {"username": USERNAME})
+
+        if existing_user:
+            flash("Username already in use")
+            return redirect(url_for("register"))
+
+        register = {
+            "Firstname": request.form.get('firstname'),
+            "Lastname": request.form.get('lastname'),            
+            "Username": USERNAME,
+            "Password": generate_password_hash(request.form.get("password")),
+            "IsAdmin": False
+        }
+        mongo.db.Users.insert_one(register)
+
+        # put the new user into session cookie
+        session["user"] = USERNAME
+        flash("Registration succesful!")
+
+    return render_template("register.html")
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         USERNAME = request.form.get("username").lower()
         PASSWORD = request.form.get("password")
         # check if username exists in db
-        existing_user = mongo.db.Users.find_one({"username": USERNAME})
+        existing_user = mongo.db.Users.find_one({"Username": USERNAME})
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(existing_user["password"], PASSWORD):
+            if check_password_hash(existing_user["Password"], PASSWORD):
                 session["user"] = USERNAME
-                flash("Welcome, {}".format(USERNAME))
+                
+                flash("Welcome {} {}".format(existing_user['Firstname'], existing_user['Lastname']))
             else:
                 # invalid password match
                 flash("Incorrect Username and\or Password")
