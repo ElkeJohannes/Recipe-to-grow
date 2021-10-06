01d57ce2d6c34d429b4b2396e30e00c44fe6737c
ELkeJohannes <elkeharmanny@outlook.com>
2021-10-06 20:59:16 +0200

    Add limited (though working) add recipe form

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import date
 import os
 from flask import (
     Flask, flash, render_template,
@@ -5,18 +6,28 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from flask import render_template
 
 if os.path.exists("env.py"):
     import env
+
+UPLOAD_FOLDER = 'static/img/recipes/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongo = PyMongo(app)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def home():
@@ -70,8 +81,10 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(existing_user["Password"], PASSWORD):
-                return redirect(url_for("myRecipes"))
+                # put the new user into session cookie
+                session["user"] = USERNAME
                 flash("Welcome {} {}".format(existing_user['Firstname'], existing_user['Lastname']))
+                return redirect(url_for("myRecipes"))                
             else:
                 # invalid password match
                 flash("Incorrect Username and\or Password")
@@ -103,6 +116,44 @@ def myRecipes():
 def viewRecipe(recipe_id):
     recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("view_recipe.html", recipe = recipe)
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def addRecipe():
+    if request.method == "POST":
+        # Get the info from the form and add some additional info
+        recipe = ({
+            "Title": request.form.get("title"),
+            "Description": request.form.get("description"),
+            "TimesViewed": 0,
+            "DateAdded": date.today().strftime("%d/%m/%Y"),
+            "Owner": session["user"]
+        })
+
+        # Handle the upload of the recipe image
+        # Credit for the code in README.md (flask.palletprojects.com)
+        # check if the post request has the file part
+        if 'recipeImage' not in request.files:
+            flash('No valid file found')
+            return redirect(request.url)
+        file = request.files['recipeImage']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = session['user'] + "_" + secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            recipe.update({
+                "Image": filename
+            })
+
+        mongo.db.Recipes.insert_one(recipe)
+        flash("Recipe added!")
+        return redirect(url_for("myRecipes"))
+
+    return render_template("add_recipe.html")
 
 
 if __name__ == "__main__":
