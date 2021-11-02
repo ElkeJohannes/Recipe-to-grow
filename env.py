cb1a3719bac634c1213d30f4abb37bd592496baa
Elke Harmanny <ElkeHarmanny@outlook.com>
2021-11-02 21:15:40 +0100

    Move secrets to seperate file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "6#r*mr8N!kycWaWI4nX8GHaO^")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:R00tUser1234@cluster0.bivzi.mongodb.net/RecipeToGrow?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "RecipeToGrow")
